83dc8f2d080f3c0bdfddee6d7c870203766c4ef8
Ben Russell <benjaminrussell36@gmail.com>
2020-07-16 11:53:11 -0700

    Add new method to check length of word being passed in by user.
    Co-authored-by: Ben White <bwhite2140@outlook.com>

--- Scrabble.Tests/Models.Tests/Scrabble.Tests.cs
+++ Scrabble.Tests/Models.Tests/Scrabble.Tests.cs
@@ -11,23 +11,56 @@ namespace Scrabble.Tests
     [TestMethod]
     public void TestConstructor_CreateInstanceOfWord_Word()
     {
-      Score newWord = new Score();
+      Score newWord = new Score("word");
       Assert.AreEqual(typeof(Score), newWord.GetType());
     }
 
     [TestMethod]
     public void GetScore_CheckIfWord_Bool()
     {
-      Score newWord = new Score();
+      Score newWord = new Score("word");
       Assert.AreEqual(true, newWord.GetScore("word"));
     }
     
     [TestMethod]
     public void AssignValues_AssignValueToLetter_Dictionary()
     {
-      Score newWord = new Score();
-      Dictionary<char, int> alphabet = new Dictionary<char, int> { {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}};
+      Score newWord = new Score("word");
+      Dictionary<char, int> alphabet = new Dictionary<char, int> { 
+      {'A', 1}, 
+      {'E', 1}, 
+      {'I', 1}, 
+      {'O', 1}, 
+      {'U', 1}, 
+      {'L', 1}, 
+      {'N', 1}, 
+      {'R', 1}, 
+      {'S', 1}, 
+      {'T', 1}, 
+      {'D', 2}, 
+      {'G', 2}, 
+      {'B', 3}, 
+      {'C', 3}, 
+      {'M', 3}, 
+      {'P', 3}, 
+      {'F', 4},
+      {'H', 4},
+      {'V', 4},
+      {'W', 4},
+      {'Y', 4},
+      {'K', 5},
+      {'J', 8},
+      {'X', 8},
+      {'Q', 10},
+      {'Z', 10}};
       CollectionAssert.AreEqual(alphabet, newWord.AssignValues());
+    }
+
+    [TestMethod]
+    public void WordLength_LengthOfWord_Int()
+    {
+    Score newWord = new Score("word");
+    Assert.AreEqual(4, newWord.WordLength());
     }
   }
 }
--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -4,14 +4,51 @@ namespace Scrabble.Models
 {
   public class Score
   {
+    public string Word {get; set;}
+
+    public Score(string word)
+    {
+      Word = word;
+    }
     public bool GetScore(string word)
     {
       return true;
     }
 
+    public int WordLength()
+    {
+      int wordLength = Word.Length;
+      return wordLength;
+    }
     public Dictionary<char, int> AssignValues()
     {
-      Dictionary<char, int> scrabble = new Dictionary<char, int> { {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}};
+      Dictionary<char, int> scrabble = new Dictionary<char, int> 
+      { {'A', 1}, 
+      {'E', 1}, 
+      {'I', 1}, 
+      {'O', 1}, 
+      {'U', 1}, 
+      {'L', 1}, 
+      {'N', 1}, 
+      {'R', 1}, 
+      {'S', 1}, 
+      {'T', 1}, 
+      {'D', 2}, 
+      {'G', 2}, 
+      {'B', 3}, 
+      {'C', 3}, 
+      {'M', 3}, 
+      {'P', 3}, 
+      {'F', 4},
+      {'H', 4},
+      {'V', 4},
+      {'W', 4},
+      {'Y', 4},
+      {'K', 5},
+      {'J', 8},
+      {'X', 8},
+      {'Q', 10},
+      {'Z', 10}};
       return scrabble;
     }
   }
